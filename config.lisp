ba396f2266792c4ad329a39010aa5acf735d1717
cxxxr <g23tlm@gmail.com>
2024-10-19 21:54:52 +0900

    update

--- config.lisp
+++ config.lisp
@@ -1,15 +1,20 @@
 (uiop:define-package :lem-rooms-client/config
   (:use #:cl
 	#:lem)
-  (:export #:access-token
-           #:base-url))
+  (:export #:base-url
+           #:access-token))
 (in-package :lem-rooms-client/config)
+
+(defparameter *base-url* "http://localhost")
+
+(defun base-url ()
+  "http://localhost")
+
+(defun (setf base-url) (base-url)
+  (setf *base-url* base-url))
 
 (defun access-token ()
   (config :rooms.access-token))
 
 (defun (setf access-token) (access-token)
   (setf (config :rooms.access-token) access-token))
-
-(defun base-url ()
-  "http://localhost")
